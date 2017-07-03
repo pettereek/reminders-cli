a07d181f565b32ac4b9dc4232fcc60a6f0ce6a4b
Petter Eek <petter@pettereek.se>
2017-07-03 21:38:23 +0200

    Assert there is a settings directory

--- Sources/DefaultList.swift
+++ Sources/DefaultList.swift
@@ -6,6 +6,7 @@ private let settingsDirectory = "Reminders CLI"
 
 final class DefaultList {
     func getDefaultListIdentifier() -> String {
+        assertFile()
         do {
             let id = try String(contentsOfFile: settingsFileURL()!.path, encoding: String.Encoding.utf8)
             return id
@@ -16,6 +17,7 @@ final class DefaultList {
     }
 
     func setDefaultList(withIdentifier id: String, listName name: String) {
+        assertFile()
         do {
             try id.write(toFile: settingsFileURL()!.path, atomically: true, encoding: String.Encoding.utf8)
             print("Using \(name.cyan)")
@@ -24,10 +26,20 @@ final class DefaultList {
         }
     }
 
+    private func assertFile() {
+        let fm = FileManager.default
+        if !fm.fileExists(atPath: settingsDirectoryURL()!.path) {
+            do {
+                try fm.createDirectory(atPath: settingsDirectoryURL()!.path, withIntermediateDirectories: true)
+            } catch {
+                print("error creating settings directory: \(error)")
+            }
+        }
+    }
+
     private func settingsDirectoryURL() -> URL? {
         let fileManager = FileManager.default
         let urls = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask)
-
         guard let applicationSupportURL = urls.first else { return nil }
         return applicationSupportURL.appendingPathComponent(settingsDirectory)
     }
